acc34fbe705430978d1a13cef73932bb1025c26a
viblo100 <49523953+viblo100@users.noreply.github.com>
2019-04-19 19:11:21 +0200

    Update MarsMap.java
    Habe eine andere Datenstruktur eingeführt. Anstatt der linkedHashMap verwende ich nun ein 2-dimensionales-Array. Es wird häufiger gebraucht als eine HashMap, was dazu führt, dass andere Leute das Programm besser verstehen können und sich nicht noch einlesen müssen. Habe die Änderungen auch in der Rover-KLasse angewandt.

--- MarsMap.java
+++ MarsMap.java
@@ -1,81 +1,45 @@
 package rover;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Random;
-import java.util.Set;
-import java.util.Map.Entry;
 
 public class MarsMap {
 	
 	static Random r = new Random();
-	static LinkedHashMap<int[], String> mars;
+	static char[][] mars;
+	static int x = 20;
+	static int y = 80;
 	
 	/*Mars Landschaft initialisieren*/
 	public static void init() {
-		mars = new LinkedHashMap<>();
-		int x = 80;
-		int y = 20;
+		mars = new char[x][y];
 		int rx = x / 2;
 		int ry = y / 2;
 		for (int i = 0; i < x; i++) {
 			for (int j = 0; j < y; j++) {
-				int[] p = new int[] { i, j };
-				if (r.nextDouble() < 0.25 && !(rx == i && ry == j))
-					mars.put(p, "#");
+				mars[i][j] = ' ';
 			}
 		}
-		mars.put(new int[] { rx, ry }, "n");
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				if (r.nextDouble() < 0.25 && !(rx == i && ry == j))
+					mars[i][j]= '#';
+			}
+		}
+		mars[rx][ry] = '^';
 	}
 	
 	/*Gibt die Marskarte aus*/
 	public static void output() {
-		int[] max = maximum(mars.keySet());
-		for (int j = 0; j < max[1]; j++) {
-			for (int i = 0; i < max[0]; i++) {
-				if (getChar(mars, new int[] { i, j }) == null) {
-					System.out.print(" ");
-					continue;
-				}
-				if (getChar(mars, new int[] { i, j }).equals("#"))
-					System.out.print("#");
-				else if (getChar(mars, new int[] { i, j }).equals("n"))
-					System.out.print("^");
-				else if (getChar(mars, new int[] { i, j }).equals("s"))
-					System.out.print("V");
-				else if (getChar(mars, new int[] { i, j }).equals("e"))
-					System.out.print(">");
-				if (getChar(mars, new int[] { i, j }).equals("w"))
-					System.out.print("<");
+		for (int j = 0; j < x; j++) {
+			for (int i = 0; i < y; i++) {
+				System.out.print(mars[j][i]);
 			}
 			System.out.println();
 		}
-		for (int i = 0; i < max[0]; i++) {
+		for (int i = 0; i < y; i++) {
 			System.out.print("=");
 		}
 		System.out.println();
 	}
-
-	public static int[] maximum(Set<int[]> set) {
-		int[] x = new int[2];
-		for (int[] e : set) {
-			if (e[0] > x[0])
-				x[0] = e[0];
-			if (e[1] > x[1])
-				x[1] = e[1];
-		}
-		return x;
-	}
-	
-	/*Gibt den char an der übergebenen Stelle zurück*/
-	public static String getChar(Map<int[], String> stones, int[] p) {
-		Set<Entry<int[], String>> entrySet = stones.entrySet();
-		for (Entry<int[], String> entry : entrySet) {
-			if (entry.getKey()[0] == p[0] && entry.getKey()[1] == p[1])
-				return entry.getValue();
-		}
-		return null;
-	}
-
-	
 }
+
