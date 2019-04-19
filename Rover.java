abad107657632cc76024337d28b7ffd84ac7b24f
viblo100 <49523953+viblo100@users.noreply.github.com>
2019-04-19 19:33:58 +0200

    Update Rover.java
    Um eine Error Message erweitert und die Variable p in pos umbenannt (position) und die Variable c über die die Argumente abgegeben werden in command (Befehl) umbenannt

--- Rover.java
+++ Rover.java
@@ -5,81 +5,87 @@ public class Rover {
 	static char[][] mars = rover.MarsMap.mars;
 	
 	/*Abfrage wie der Rover sich bewegen muss, nach den übergebenen Argumenten*/
-	public static void moveRover(char c) {
-		int[] p = searchRover();
-		if (c == 'f') {
-			goForward(p);									
-		} else if (c == 'b') {
-			goBackward(p);		
-		} else if (c == 'l') {
-			goLeft(p);
-		} else if (c == 'r') {
-			goRight(p);
+	public static void moveRover(char command) {
+		int[] pos = searchRover();
+		if (command == 'f') {
+			goForward(pos);									
+		} else if (command== 'b') {
+			goBackward(pos);		
+		} else if (command == 'l') {
+			goLeft(pos);
+		} else if (command == 'r') {
+			goRight(pos);
 		}
 	}
 	
-	public static void goForward(int[] p) {
-		if(p[0] > 0 && p[1] > 0 && p[0] < rover.MarsMap.x -1 && p[1] < rover.MarsMap.y -1 ) {
-			if (mars[p[0]][p[1]] == '^' && !(mars[p[0]-1][p[1]] == '#')) {
-				mars[p[0]-1][p[1]] = '^';
-				mars[p[0]][p[1]] = ' ';
+	public static void goForward(int[] pos) {
+		if(pos[0] > 0 && pos[1] > 0 && pos[0] < rover.MarsMap.x -1 && pos[1] < rover.MarsMap.y -1 ) {	//Abfrage ob der Rover am Rand der Karte steht.
+			if (mars[pos[0]][pos[1]] == '^' && !(mars[pos[0]-1][pos[1]] == '#')) {
+				mars[pos[0]-1][pos[1]] = '^';
+				mars[pos[0]][pos[1]] = ' ';
 			}
-			else if (mars[p[0]][p[1]] == 'V' && !(mars[p[0]+1][p[1]] == '#')) {
-				mars[p[0]+1][p[1]] = 'V';
-				mars[p[0]][p[1]] = ' ';	
+			else if (mars[pos[0]][pos[1]] == 'V' && !(mars[pos[0]+1][pos[1]] == '#')) {
+				mars[pos[0]+1][pos[1]] = 'V';
+				mars[pos[0]][pos[1]] = ' ';	
 			}
-			else if (mars[p[0]][p[1]] == '<' && !(mars[p[0]][p[1]-1] == '#')) {
-				mars[p[0]][p[1]-1] = '<';
-				mars[p[0]][p[1]] = ' ';
+			else if (mars[pos[0]][pos[1]] == '<' && !(mars[pos[0]][pos[1]-1] == '#')) {
+				mars[pos[0]][pos[1]-1] = '<';
+				mars[pos[0]][pos[1]] = ' ';
 			}
-			else if (mars[p[0]][p[1]] == '>' && !(mars[p[0]][p[1]+1] == '#')) {
-					mars[p[0]][p[1]+1] = '>';
-					mars[p[0]][p[1]] = ' ';
+			else if (mars[pos[0]][pos[1]] == '>' && !(mars[pos[0]][pos[1]+1] == '#')) {
+					mars[pos[0]][pos[1]+1] = '>';
+					mars[pos[0]][pos[1]] = ' ';
 			}
+		} else {
+			System.out.println("ERROR: Rover reached end of Map!");
+			System.exit(0);
 		}
 	}
 	
-	public static void goBackward(int[] p) {
-		if(p[0] > 0 && p[1] > 0 && p[0] < rover.MarsMap.x -1 && p[1] < rover.MarsMap.y -1 ) {
-			if (mars[p[0]][p[1]] == '^' && !(mars[p[0]+1][p[1]] == '#')) {
-				mars[p[0]+1][p[1]] = '^';
-				mars[p[0]][p[1]] = ' ';
+	public static void goBackward(int[] pos) {
+		if(pos[0] > 0 && pos[1] > 0 && pos[0] < rover.MarsMap.x -1 && pos[1] < rover.MarsMap.y -1 ) {	//Abfrage ob der Rover am Rand der Karte steht.
+			if (mars[pos[0]][pos[1]] == '^' && !(mars[pos[0]+1][pos[1]] == '#')) {
+				mars[pos[0]+1][pos[1]] = '^';
+				mars[pos[0]][pos[1]] = ' ';
 			}
-			else if (mars[p[0]][p[1]] == 'V' && !(mars[p[0]-1][p[1]] == '#')) {
-				mars[p[0]-1][p[1]] = 'V';
-				mars[p[0]][p[1]] = ' ';	
+			else if (mars[pos[0]][pos[1]] == 'V' && !(mars[pos[0]-1][pos[1]] == '#')) {
+				mars[pos[0]-1][pos[1]] = 'V';
+				mars[pos[0]][pos[1]] = ' ';	
 			}
-			else if (mars[p[0]][p[1]] == '<' && !(mars[p[0]][p[1]+1] == '#')) {
-				mars[p[0]][p[1]+1] = '<';
-				mars[p[0]][p[1]] = ' ';
+			else if (mars[pos[0]][pos[1]] == '<' && !(mars[pos[0]][pos[1]+1] == '#')) {
+				mars[pos[0]][pos[1]+1] = '<';
+				mars[pos[0]][pos[1]] = ' ';
 			}
-			else if (mars[p[0]][p[1]] == '>' && !(mars[p[0]][p[1]-1] == '#')) {
-					mars[p[0]][p[1]-1] = '>';
-					mars[p[0]][p[1]] = ' ';
+			else if (mars[pos[0]][pos[1]] == '>' && !(mars[pos[0]][pos[1]-1] == '#')) {
+					mars[pos[0]][pos[1]-1] = '>';
+					mars[pos[0]][pos[1]] = ' ';
 			}
+		} else {
+			System.out.println("ERROR: Rover reached end of Map!");
+			System.exit(0);
 		}
 	}
 	
-	public static void goLeft(int[] p) {
-		if (mars[p[0]][p[1]] == '^')
-			mars[p[0]][p[1]] = '<';
-		else if (mars[p[0]][p[1]] == '<')
-			mars[p[0]][p[1]] = 'V';
-		else if (mars[p[0]][p[1]] == 'V')
-			mars[p[0]][p[1]] = '>';
-		else if (mars[p[0]][p[1]] == '>')
-			mars[p[0]][p[1]] = '^';
+	public static void goLeft(int[] pos) {
+		if (mars[pos[0]][pos[1]] == '^')
+			mars[pos[0]][pos[1]] = '<';
+		else if (mars[pos[0]][pos[1]] == '<')
+			mars[pos[0]][pos[1]] = 'V';
+		else if (mars[pos[0]][pos[1]] == 'V')
+			mars[pos[0]][pos[1]] = '>';
+		else if (mars[pos[0]][pos[1]] == '>')
+			mars[pos[0]][pos[1]] = '^';
 	}
 	
-	public static void goRight(int[] p) {
-		if (mars[p[0]][p[1]] == '^')
-			mars[p[0]][p[1]] = '>';
-		else if (mars[p[0]][p[1]] == '<')
-			mars[p[0]][p[1]] = '^';
-		else if (mars[p[0]][p[1]] == 'V')
-			mars[p[0]][p[1]] = '<';
-		else if (mars[p[0]][p[1]] == '>')
-			mars[p[0]][p[1]] = 'V';
+	public static void goRight(int[] pos) {
+		if (mars[pos[0]][pos[1]] == '^')
+			mars[pos[0]][pos[1]] = '>';
+		else if (mars[pos[0]][pos[1]] == '<')
+			mars[pos[0]][pos[1]] = '^';
+		else if (mars[pos[0]][pos[1]] == 'V')
+			mars[pos[0]][pos[1]] = '<';
+		else if (mars[pos[0]][pos[1]] == '>')
+			mars[pos[0]][pos[1]] = 'V';
 	}
 	
 	/*Sucht den Rover und gibt die aktuelle Position zurück*/
